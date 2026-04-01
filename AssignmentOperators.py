a9bea823f15e4925ada2a118e490342479355492
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2026-04-01 23:46:06 +0530

    first commit

---
+++ AssignmentOperators.py
@@ -0,0 +1,13 @@
+num = 5
+
+num += 3   
+print(num) 
+
+num -= 2   
+print(num) 
+
+num *= 2
+print(num) 
+
+num /= 3
+print(num) 
---
+++ LogicalOperators.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+c = 20
+
+
+print("AND Examples:")
+print(a > b and c > a)   
+print(a > b and c < a)   
+
+
+print("\nOR Examples:")
+print(a > b or c < a)    
+print(a < b or c < a)    
+
+print("\nNOT Examples:")
+print(not(a > b))        
+print(not(a < b))        
